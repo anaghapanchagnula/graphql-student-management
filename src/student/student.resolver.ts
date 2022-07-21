96ac71e5c4bb6dd43ede473952c8f2d0992c2f68
Anagha Panchagnula <anaghapanchagnula@anaghas-mbp.lan>
2022-07-21 13:36:22 -0700

    init commit

---
+++ src/student/student.resolver.ts
@@ -0,0 +1,28 @@
+import { Query } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { LessonType } from 'src/lesson/lesson.types';
+import { CreateStudentInput } from './create-student.input';
+import { StudentService } from './student.service';
+import { StudentType } from './student.type';
+
+@Resolver((of) => StudentType)
+export class StudentResolver {
+  constructor(private studentService: StudentService) {}
+
+  @Query((returns) => [StudentType])
+  async students() {
+    return this.studentService.getAllStudents();
+  }
+
+  @Query((returns) => StudentType)
+  async student(@Args('id') id: string) {
+    return this.studentService.getStudent(id);
+  }
+
+  @Mutation((returns) => StudentType)
+  async createStudent(
+    @Args('createStudentInput') createStudentInput: CreateStudentInput,
+  ) {
+    return this.studentService.createStudent(createStudentInput);
+  }
+}
